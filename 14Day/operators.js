986a6c9a7c6042b7373a7892c020002ebfdfdbf8
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-01-10 10:04:47 +0500

    opeators & number data type

---
+++ 14Day/operators.js
@@ -0,0 +1,8 @@
+a = 20;
+b = 30;
+
+console.log("Addition of A + B: ",a + b);
+console.log("Subtraction of A + B: ",a - b);
+console.log("Division of A + B: ",a / b);
+console.log("Modulo of A + B: ",a % b);
+console.log("Power of A + B: ",a ** b);
---
+++ 14Day/script.js
@@ -0,0 +1,9 @@
+
+// numbers data type in js
+a = 20;
+console.log(a);
+console.log(typeof(a));
+
+b = "Hello";
+console.log(b);
+console.log(typeof(b));
