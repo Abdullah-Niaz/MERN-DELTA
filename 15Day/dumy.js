3c1d6f2b34225b8edbffd04031a6caca764cdc27
Abdullah-Niaz <abdullahniaz423@gmail.com>
2024-01-22 19:22:20 +0500

    Node Package

---
+++ 15Day/dumy.js
@@ -0,0 +1,12 @@
+let isTimeToCode = true;
+let needsCoffee = true;
+
+
+//  if(!true && true)
+        (false && true)
+if (!isTimeToCode && needsCoffee) {
+    console.log("Make some coffee! It's time to code!");
+}
+else{
+    console.log("No");
+}
